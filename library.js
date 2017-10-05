fdf2c37c1fbdcfbe52ba528fcb3414c659efbf01
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-04 17:10:58 -0700

    Defined getOverallRating() and getTotalDuration() functions, getTrackLength() and getTrackRating() methods

--- library.js
+++ library.js
@@ -8,6 +8,7 @@ let MusicLibrary = function (name, creator) {
     this.playlists = [];
 };
 
+    // Add a playlist object to the MusicLibrary
     MusicLibrary.prototype.addPlaylist = function (playlist) {
         return this.playlists.push(playlist);
     };
@@ -22,31 +23,49 @@ let Playlist = function (name) {
     this.tracks = [];
 }
 
-    Playlist.prototype.addTrack = function(track) {
+    Playlist.prototype.addTrack = function (track) {
         this.tracks.push(track);
     }
 
-    Playlist.prototype.overallRating = function() {
+    Playlist.prototype.getOverallRating = function () {
+        let totalRating = 0;
 
+        this.tracks.forEach((track) => {
+            totalRating += track.getTrackRating();
+        });
+
+        return totalRating / this.tracks.length;
     }
 
-    Playlist.prototype.totalDuration = function() {
+    Playlist.prototype.getTotalDuration = function () {
+        let duration = 0;
 
+        this.tracks.forEach((track) => {
+            duration += track.getTrackLength();
+        });
+
+        return duration;
     }
 
 /*
  *  Track class definition
  */
 
-let Track = function(title, rating, length) {
-    let track = {
+let Track = function (title, rating, length) {
+    this.track = {
         title: title,
         rating: rating,
         length: length
     }
-    return track;
 }
 
+    Track.prototype.getTrackLength = function () {
+        return this.track.length;
+    }
+
+    Track.prototype.getTrackRating = function () {
+        return this.track.rating;
+    }
 
 
 /*
@@ -57,18 +76,18 @@ let Track = function(title, rating, length) {
 let myLibrary = new MusicLibrary('Chads Playlist', 'Chadder');
 
 let newPlaylist = new Playlist('2017 Favorite Tunes');
-    newPlaylist.addTrack( new Track('Sunshine Lollipops', 5, 3.29) );
-    newPlaylist.addTrack( new Track('Sunshine Lollipops', 2, 2.29) );
-    newPlaylist.addTrack( new Track('Sunshine Lollipops', 6, 3.19) );
-    newPlaylist.addTrack( new Track('Sunshine Lollipops', 9, 5.45) );
+    newPlaylist.addTrack(new Track('Sunshine Lollipops', 5, 243));
+    newPlaylist.addTrack(new Track('Sunshine Lollipops', 2, 229));
+    newPlaylist.addTrack(new Track('Sunshine Lollipops', 1, 319));
+    newPlaylist.addTrack(new Track('Sunshine Lollipops', 3, 545));
 
 let anotherPlaylist = new Playlist('2018 Tunes to Jam');
-    anotherPlaylist.addTrack( new Track('Track 1', 5, 2.45) );
-    anotherPlaylist.addTrack( new Track('Track 2', 4, 1.45) );
-    anotherPlaylist.addTrack( new Track('Track 3', 7, 5.45) );
-    anotherPlaylist.addTrack( new Track('Track 4', 2, 4.15) );
+    anotherPlaylist.addTrack(new Track('Track 1', 5, 245));
+    anotherPlaylist.addTrack(new Track('Track 2', 4, 145));
+    anotherPlaylist.addTrack(new Track('Track 3', 1, 545));
+    anotherPlaylist.addTrack(new Track('Track 4', 1, 415));
     anotherPlaylist.getOverallRating(); // return overallRating
     anotherPlaylist.getTotalDuration(); // return totalDuration
 
-myLibrary.addPlaylist( newPlaylist );
-myLibrary.addPlaylist( anotherPlaylist );
+myLibrary.addPlaylist(newPlaylist);
+myLibrary.addPlaylist(anotherPlaylist);
